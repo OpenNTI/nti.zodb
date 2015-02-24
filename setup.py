4c59c5a81e680842ccc9dfbb7fb78443056130de
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-24 20:32:39 +0000

    nti.schema is not py3 ready
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.zodb/trunk@60117 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -8,67 +8,65 @@ py_impl = getattr(platform, 'python_implementation', lambda: None)
 IS_PYPY = py_impl() == 'PyPy'
 
 entry_points = {
-    'console_scripts': [
-    ],
+	'console_scripts': [
+	],
 }
 
 TESTS_REQUIRE = [
-    'fudge',
-    'nose',
-    'nose-timer',
-    'nose-pudb',
-    'nose-progressive',
-    'nose2[coverage_plugin]',
-    'pyhamcrest',
-    'zope.testing',
-    'nti.testing',
-    'nti.nose_traceback_info',
+	'fudge',
+	'nose',
+	'nose-timer',
+	'nose-pudb',
+	'nose-progressive',
+	'nose2[coverage_plugin]',
+	'pyhamcrest',
+	'zope.testing',
+	'nti.testing'
 ]
 
 setup(
-    name='nti.zodb',
-    version=VERSION,
-    author='Jason Madden',
-    author_email='jason@nextthought.com',
-    description="NTI ZODB",
-    long_description=codecs.open('README.rst', encoding='utf-8').read(),
-    license='Proprietary',
-    keywords='ZODB',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
+	name='nti.zodb',
+	version=VERSION,
+	author='Jason Madden',
+	author_email='jason@nextthought.com',
+	description="NTI ZODB",
+	long_description=codecs.open('README.rst', encoding='utf-8').read(),
+	license='Proprietary',
+	keywords='ZODB',
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
 		'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: Implementation :: CPython'
-    ],
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: Implementation :: CPython'
+	],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
 	namespace_packages=['nti'],
-    tests_require=TESTS_REQUIRE,
+	tests_require=TESTS_REQUIRE,
 	install_requires=[
 		'setuptools',
-        'perfmetrics',
-        'repoze.zodbconn' if not IS_PYPY else '', 
-        'zc.zlibstorage',
-        'ZODB',
-        'zope.component',
-        'zope.copy',
-        'zope.file',
-        'zope.interface',
-        'zope.minmax',
-        'zope.security',
-        'nti.common',
-        'nti.schema'
+		'perfmetrics',
+		'repoze.zodbconn' if not IS_PYPY else '', 
+		'zc.zlibstorage',
+		'ZODB',
+		'zope.component',
+		'zope.copy',
+		'zope.file',
+		'zope.interface',
+		'zope.minmax',
+		'zope.security',
+		'nti.common',
+		'nti.schema',
+        'nti.nose_traceback_info'
 	],
-    extras_require={
-        'test': TESTS_REQUIRE,
-    },
-    dependency_links=[
-        'git+https://github.com/NextThought/nti.schema.git#egg=nti.schema',
-        'git+https://github.com/NextThought/nti.nose_traceback_info.git#egg=nti.nose_traceback_info'
-    ],
+	extras_require={
+		'test': TESTS_REQUIRE,
+	},
+	dependency_links=[
+		'git+https://github.com/NextThought/nti.schema.git#egg=nti.schema',
+		'git+https://github.com/NextThought/nti.nose_traceback_info.git#egg=nti.nose_traceback_info'
+	],
 	entry_points=entry_points
 )
