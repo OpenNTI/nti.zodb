0b2b4f5af5dffd21fd8139b23adf361c2c0af4b7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-10-23 00:55:01 +0000

    pylint
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.zodb/trunk@99244 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/zodb/minmax.py
+++ src/nti/zodb/minmax.py
@@ -15,7 +15,9 @@ import functools
 
 from zope import interface
 
-from zope.minmax._minmax import Maximum, Minimum, AbstractValue
+from zope.minmax._minmax import Maximum
+from zope.minmax._minmax import Minimum
+from zope.minmax._minmax import AbstractValue
 
 from nti.zodb.interfaces import INumericValue
 from nti.zodb.interfaces import INumericCounter
@@ -24,7 +26,7 @@ from nti.zodb.interfaces import INumericCounter
 # to potentially override
 def _set(self, value):
 	self.value = value
-assert 'set' not in AbstractValue.__dict__ or AbstractValue.set is _set # catch incompatible changes
+assert 'set' not in AbstractValue.__dict__ or AbstractValue.set is _set  # catch incompatible changes
 AbstractValue.set = _set
 
 @functools.total_ordering
@@ -36,26 +38,26 @@ class AbstractNumericValue(AbstractValue):
 	"""
 	value = 0
 
-	def __init__( self, value=0 ):
-		super(AbstractNumericValue,self).__init__( value )
+	def __init__(self, value=0):
+		super(AbstractNumericValue, self).__init__(value)
 
 	# Comparison methods
-	def __eq__( self, other ):
+	def __eq__(self, other):
 		try:
 			return other is self or self.value == other.value
-		except AttributeError: # pragma: no cover
+		except AttributeError:  # pragma: no cover
 			return NotImplemented
 
-	def __lt__(self, other ):
+	def __lt__(self, other):
 		try:
 			return self.value < other.value
-		except AttributeError: # pragma: no cover
+		except AttributeError:  # pragma: no cover
 			return NotImplemented
 
-	def __gt__(self,other):
+	def __gt__(self, other):
 		try:
 			return self.value > other.value
-		except AttributeError: # pragma: no cover
+		except AttributeError:  # pragma: no cover
 			return NotImplemented
 
 	def __hash__(self):
@@ -63,31 +65,31 @@ class AbstractNumericValue(AbstractValue):
 
 	# Numeric methods
 	def __isub__(self, other):
-		other_value = getattr( other, 'value', other )
-		self.set( self.value - other_value )
+		other_value = getattr(other, 'value', other)
+		self.set(self.value - other_value)
 		return self
 
 	def __iadd__(self, other):
-		other_value = getattr( other, 'value', other )
-		self.set( self.value + other_value )
+		other_value = getattr(other, 'value', other)
+		self.set(self.value + other_value)
 		return self
 
-	def __rsub__( self, other ):
+	def __rsub__(self, other):
 		# other - self.
 		# By definition, not called if other is the same type as this
 		return other - self.value
 
-	def __add__( self, other ):
-		other_value = getattr( other, 'value', other )
+	def __add__(self, other):
+		other_value = getattr(other, 'value', other)
 		result = self.value + other_value
 		if other_value is not other:
 			result = type(self)(result)
 		return result
 
-	def __str__( self ):
+	def __str__(self):
 		return str(self.value)
 
-	def __repr__( self ):
+	def __repr__(self):
 		return "%s(%s)" % (self.__class__.__name__, self.value)
 
 class _ConstantZeroValue(AbstractNumericValue):
@@ -96,20 +98,20 @@ class _ConstantZeroValue(AbstractNumericValue):
 	value cannot be changed, and instances cannot be serialized.
 	"""
 
-	def __init__( self, value=0 ):
-		super(_ConstantZeroValue,self).__init__( value=0 )
+	def __init__(self, value=0):
+		super(_ConstantZeroValue, self).__init__(value=0)
 		assert 'value' not in self.__dict__
 
-	def __getstate__( self ):
+	def __getstate__(self):
 		raise TypeError()
 
 	def _p_resolveConflict(self, old, committed, new):
 		raise NotImplementedError()
 
-	def set( self, value ):
+	def set(self, value):
 		pass
 
-	value = property( lambda s: 0, lambda s, nv: None )
+	value = property(lambda s: 0, lambda s, nv: None)
 
 _czv = _ConstantZeroValue()
 
@@ -121,7 +123,7 @@ class NumericMaximum(AbstractNumericValue, Maximum):
 	"""
 	Maximizes the number during conflicts.
 	"""
-_SafeMaximum = NumericMaximum # BWC
+_SafeMaximum = NumericMaximum  # BWC
 
 class NumericMinimum(AbstractNumericValue, Minimum):
 	"""
@@ -141,13 +143,13 @@ class MergingCounter(AbstractNumericValue):
 		self.value += amount
 		return self
 
-	def _p_resolveConflict( self, oldState, savedState, newState ):
+	def _p_resolveConflict(self, oldState, savedState, newState):
 		saveDiff = savedState - oldState
 		newDiff = newState - oldState
 		savedState = oldState + saveDiff + newDiff
 		return savedState
 
-from .persistentproperty import PropertyHoldingPersistent
+from nti.zodb.persistentproperty import PropertyHoldingPersistent
 
 class NumericPropertyDefaultingToZero(PropertyHoldingPersistent):
 	"""
@@ -158,8 +160,8 @@ class NumericPropertyDefaultingToZero(PropertyHoldingPersistent):
 	@interface.implementer(INumericCounter)
 	class IncrementingZeroValue(_ConstantZeroValue):
 
-		def __init__( self, name, holder, prop ):
-			_ConstantZeroValue.__init__( self )
+		def __init__(self, name, holder, prop):
+			_ConstantZeroValue.__init__(self)
 			self.__name__ = name
 			self.holder = holder
 			self.prop = prop
@@ -167,17 +169,17 @@ class NumericPropertyDefaultingToZero(PropertyHoldingPersistent):
 		def increment(self, amount=1):
 			# Use the original NumericPropertyDefaultingToZero descriptor
 			# to set the value, calling the factory and storing it.
-			self.prop.__set__( self.holder, amount )
-			return self.prop.__get__( self.holder, type(self.holder) )
+			self.prop.__set__(self.holder, amount)
+			return self.prop.__get__(self.holder, type(self.holder))
 
-		def set( self, value ):
+		def set(self, value):
 			if value == 0:
 				return
-			self.prop.__set__( self.holder, value )
+			self.prop.__set__(self.holder, value)
 
 
 	as_number = False
-	def __init__( self, name, factory, as_number=False ):
+	def __init__(self, name, factory, as_number=False):
 		"""
 		Creates a new property in a new-style class that does not use ``__slots__``
 		(persistent classes shouldn't use ``__slots__`` anyway).
@@ -195,14 +197,14 @@ class NumericPropertyDefaultingToZero(PropertyHoldingPersistent):
 			want to access its ``.value`` attribute. Setting this property always
 			takes the (raw) numeric value.
 		"""
-		if not isinstance(name, str): # force native string
+		if not isinstance(name, str):  # force native string
 			raise ValueError("name must be native string")
 		self.__name__ = name
 		self.factory = factory
 		if as_number:
 			self.as_number = True
 
-	def __activate( self, inst ):
+	def __activate(self, inst):
 		"we must activate objects before accessing their dict otherwise it may not be loaded"
 		try:
 			inst._p_activate()
@@ -218,33 +220,33 @@ class NumericPropertyDefaultingToZero(PropertyHoldingPersistent):
 		except AttributeError:
 			pass
 
-	def __get__( self, inst, klass ):
+	def __get__(self, inst, klass):
 		if inst is None:
 			return self
 
-		self.__activate( inst )
+		self.__activate(inst)
 		if self.__name__ in inst.__dict__:
 			value = inst.__dict__[self.__name__]
 			return value.value if self.as_number else value
 
-		return 0 if self.as_number else self.IncrementingZeroValue( self.__name__, inst, self )
+		return 0 if self.as_number else self.IncrementingZeroValue(self.__name__, inst, self)
 
-	def __set__( self, inst, value ):
-		self.__activate( inst )
-		val = inst.__dict__.get( self.__name__, None )
+	def __set__(self, inst, value):
+		self.__activate(inst)
+		val = inst.__dict__.get(self.__name__, None)
 		if val is None or isinstance(val, int):
 			if not value:
-				return # not in dict, but they gave us the default value, so ignore it
-			val = self.factory( value )
+				return  # not in dict, but they gave us the default value, so ignore it
+			val = self.factory(value)
 			inst.__dict__[self.__name__] = val
 			self.__changed(inst)
 			if getattr(inst, '_p_jar', None) is not None:
-				inst._p_jar.add( val )
+				inst._p_jar.add(val)
 		else:
-			val.set( value )
+			val.set(value)
 
-	def __delete__( self, inst ):
-		self.__activate( inst )
+	def __delete__(self, inst):
+		self.__activate(inst)
 		if self.__name__ in inst.__dict__:
 			del inst.__dict__[self.__name__]
 			self.__changed(inst)
