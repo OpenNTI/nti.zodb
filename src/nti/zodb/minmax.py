6542d9d15a84ac4f65ad256da72d548996572527
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-22 20:40:11 +0000

    Use IConnection
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.zodb/trunk@111204 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/zodb/minmax.py
+++ src/nti/zodb/minmax.py
@@ -19,6 +19,8 @@ from zope.minmax._minmax import Maximum
 from zope.minmax._minmax import Minimum
 from zope.minmax._minmax import AbstractValue
 
+from ZODB.interfaces import IConnection
+
 from nti.zodb.interfaces import INumericValue
 from nti.zodb.interfaces import INumericCounter
 
@@ -257,8 +259,9 @@ class NumericPropertyDefaultingToZero(PropertyHoldingPersistent):
             val = self.factory(value)
             inst.__dict__[self.__name__] = val
             self.__changed(inst)
-            if getattr(inst, '_p_jar', None) is not None:
-                inst._p_jar.add(val)
+            connection = IConnection(inst, None)
+            if connection is not None:
+                connection.add(val)
         else:
             val.set(value)
 
