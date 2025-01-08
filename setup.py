50ca3499478592d241227861809b6b39718cd6db
Jason Madden <jamadden@gmail.com>
2025-01-08 08:33:35 -0600

    Back to development: 4.5.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def _read(fname):
 
 setup(
     name='nti.zodb',
-    version='4.5.0',
+    version='4.5.1.dev0',
     author='Jason Madden',
     author_email='jason@nextthought.com',
     description="Utilities for ZODB",
